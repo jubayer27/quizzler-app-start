fc09ee72fe699633e092f9e9ac038b2bd6877240
MD JUBAYER ISLAM <jubayerislam2702@gmail.com>
2025-08-05 04:01:39 +0800

    Quiz app using

--- data.py
+++ data.py
@@ -2,10 +2,10 @@
 
 import requests
 parameters = {
-    "amount": 5,
-    "category": 9,  # General Knowledge
+    "amount": 20, #change this to get more or fewer questions
+    "category": 9,  # change this to get questions from different categories
 
-    "type": "boolean"
+    "type": "boolean" # change this to "multiple" for multiple choice questions
 }
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()  # Ensure we raise an error for bad responses
